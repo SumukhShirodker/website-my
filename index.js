11f06bf8441bae4583394a8b3b4e2cd03a447268
SumukhShirodker <sumukhshirodker@gmail.com>
2019-02-21 09:21:29 +0530

    Used Webp instead of PNG

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 const express = require('express')
 const app = express()
 app.set('view engine','ejs')
-
 //main code
 app.get('/',(req,res)=>{
   res.render('index')
 })
-//static stuff
 app.get('/static/htmlplusstylescss',(req,res)=>{
   res.sendFile(__dirname+'/src/css/styles.css')
 })
 app.get('/static/_java_script',(req,res)=>{
   res.sendFile(__dirname+'/src/js/app.js')
 })
-app.get('/static/images/image/yt',(req,res)=>{
-  res.sendFile(__dirname+'/src/images/yt.png')
-})
 app.get('/static/images/image/insta',(req,res)=>{
-  res.sendFile(__dirname+'/src/images/insta.png')
+  res.sendFile(__dirname+'/src/images/insta.webp')
 })
 app.get('/static/images/image/github',(req,res)=>{
-  res.sendFile(__dirname+'/src/images/gh.png')
+  res.sendFile(__dirname+'/src/images/gh.webp')
+})
+app.get('/static/images/image/yt',(req,res)=>{
+  res.sendFile(__dirname+'/src/images/yt.webp')
 })
 //listenig ports
 app.listen(7070,()=>{
